dd6d7a2d8bed21651cb28d5e8396197605e51877
Juan Hernandez <juan.hernandez@redhat.com>
2022-01-28 15:17:54 +0100

    Update to Ginkgo 2.1.1
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

--- provider/identity_provider_resource_test.go
+++ provider/identity_provider_resource_test.go
@@ -19,7 +19,7 @@ package provider
 import (
 	"net/http"
 
-	. "github.com/onsi/ginkgo/v2"                      // nolint
+	. "github.com/onsi/ginkgo/v2/dsl/core"             // nolint
 	. "github.com/onsi/gomega"                         // nolint
 	. "github.com/onsi/gomega/ghttp"                   // nolint
 	. "github.com/openshift-online/ocm-sdk-go/testing" // nolint

d447ed3e30e5eb30228a6342d5e247edfc49575d
Danielle Barda <dbarda@redhat.com>
2023-03-07 17:29:25 +0200

    Refactoring fullcycle test

--- ci/e2e/terraform_provider_ocm_files/variables.tf
+++ ci/e2e/terraform_provider_ocm_files/variables.tf
@@ -19,15 +19,3 @@ variable account_role_prefix {
 variable cluster_name {
     type = string
 }
-
-variable ocm_env {
-    type = string
-    default = "staging"
-}
-
-
-variable rosa_openshift_version {
-    type = string
-    default = "4.12"
-}
-

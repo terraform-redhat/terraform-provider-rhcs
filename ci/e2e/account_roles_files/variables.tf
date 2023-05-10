b5e18dcbb442bf29b30b751d24159c2399e027e3
Asher Shoshan <ashoshan@redhat.com>
2023-05-10 18:57:16 +0300

    init

--- ci/e2e/account_roles_files/variables.tf
+++ ci/e2e/account_roles_files/variables.tf
@@ -1,15 +1,21 @@
-variable account_role_prefix {
-    type = string
-}
-
-variable ocm_env {
+variable ocm_environment {
     type = string
     default = "staging"
 }
 
-
-variable rosa_openshift_version {
+variable openshift_version {
     type = string
-    default = "4.12"
+    default = "4.13"
 }
 
+variable account_role_prefix {
+    type = string
+}
+
+variable token {
+    type = string
+}
+
+variable url {
+    type = string
+}
--- ci/e2e/terraform_provider_ocm_files/variables.tf
+++ ci/e2e/terraform_provider_ocm_files/variables.tf
@@ -19,3 +19,18 @@ variable account_role_prefix {
 variable cluster_name {
     type = string
 }
+
+variable aws_region {
+    type = string
+    default = "us-east-1"
+}
+
+variable aws_availability_zones {
+    type = string
+    default = "us-east-1a"
+}
+
+variable replicas {
+    type = string
+    default = "3"
+}

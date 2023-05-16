3cb73974aeea2187f562d9b62936ae8766048f48
Igal Tsoiref <itsoiref@redhat.com>
2023-05-16 15:34:13 +0300

    Fixing issue with empty aws_http_tokens in ocm responce

--- provider/cluster_rosa_classic_resource.go
+++ provider/cluster_rosa_classic_resource.go
@@ -1399,7 +1399,7 @@ func populateRosaClassicClusterState(ctx context.Context, object *cmv1.Cluster, 
 	}
 
 	httpTokensState, ok := object.AWS(***REMOVED***.GetHttpTokensState(***REMOVED***
-	if ok {
+	if ok && httpTokensState != "" {
 		state.HttpTokensState = types.String{
 			Value: string(httpTokensState***REMOVED***,
 ***REMOVED***

2a3c7ab89b64e7856708a9fbc7f81457ae399659
Sagi Dayan <sagidayan@gmail.com>
2023-07-05 12:12:56 +0300

    Rename in the example output of cluster sts
    Signed-off-by: Sagi Dayan <sagidayan@gmail.com>

--- examples/create_rosa_cluster/create_rosa_sts_cluster/classic_sts/cluster/output.tf
+++ examples/create_rosa_cluster/create_rosa_sts_cluster/classic_sts/cluster/output.tf
@@ -1,11 +1,12 @@
 output "cluster_id" {
-  value = ocm_cluster_rosa_classic.rosa_sts_cluster.id
+  value = rhcs_cluster_rosa_classic.rosa_sts_cluster.id
 }
 
 output "oidc_thumbprint" {
-  value = ocm_cluster_rosa_classic.rosa_sts_cluster.sts.thumbprint
+  value = rhcs_cluster_rosa_classic.rosa_sts_cluster.sts.thumbprint
 }
 
 output "oidc_endpoint_url" {
-  value = ocm_cluster_rosa_classic.rosa_sts_cluster.sts.oidc_endpoint_url
+  value = rhcs_cluster_rosa_classic.rosa_sts_cluster.sts.oidc_endpoint_url
 }
+

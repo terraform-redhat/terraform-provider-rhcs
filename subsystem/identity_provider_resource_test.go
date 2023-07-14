fda2dffb6e1f9cc25db5ee94c1a46ee0c1355966
John Strunk <jstrunk@redhat.com>
2023-07-14 12:49:08 -0400

    Add tests for import of IDP
    Signed-off-by: John Strunk <jstrunk@redhat.com>

--- subsystem/identity_provider_resource_test.go
+++ subsystem/identity_provider_resource_test.go
@@ -972,3 +972,117 @@ var _ = Describe("Identity provider creation", func(***REMOVED*** {
 ***REMOVED******REMOVED***
 	}***REMOVED***
 }***REMOVED***
+
+var _ = Describe("Identity provider import", func(***REMOVED*** {
+	It("Can import an identity provider", func(***REMOVED*** {
+		// Prepare the server:
+		server.AppendHandlers(
+			// List IDPs to map name to ID:
+			CombineHandlers(
+				VerifyRequest(
+					http.MethodGet,
+					"/api/clusters_mgmt/v1/clusters/123/identity_providers",
+				***REMOVED***,
+				RespondWithJSON(http.StatusOK, `{
+					"kind": "IdentityProviderList",
+					"href": "/api/clusters_mgmt/v1/clusters/24vg6o424djht8h6lpoli2urg69t7vnt/identity_providers",
+					"page": 1,
+					"size": 1,
+					"total": 1,
+					"items": [
+						{
+						"kind": "IdentityProvider",
+						"type": "GithubIdentityProvider",
+						"href": "/api/clusters_mgmt/v1/clusters/24vg6o424djht8h6lpoli2urg69t7vnt/identity_providers/24vgs9hgnl5bukujvkcmgkvfgc01ss0r",
+						"id": "24vgs9hgnl5bukujvkcmgkvfgc01ss0r",
+						"name": "my-ip",
+						"mapping_method": "claim",
+						"github": {
+							"client_id": "99999",
+							"organizations": [
+								"myorg"
+							]
+				***REMOVED***
+				***REMOVED***
+					]
+		***REMOVED***`***REMOVED***,
+			***REMOVED***,
+			// Read the IDP to load the current state:
+			CombineHandlers(
+				VerifyRequest(
+					http.MethodGet,
+					"/api/clusters_mgmt/v1/clusters/123/identity_providers/24vgs9hgnl5bukujvkcmgkvfgc01ss0r",
+				***REMOVED***,
+				RespondWithJSON(http.StatusOK, `{
+					"kind": "IdentityProvider",
+					"type": "GithubIdentityProvider",
+					"href": "/api/clusters_mgmt/v1/clusters/24vg6o424djht8h6lpoli2urg69t7vnt/identity_providers/24vgs9hgnl5bukujvkcmgkvfgc01ss0r",
+					"id": "24vgs9hgnl5bukujvkcmgkvfgc01ss0r",
+					"name": "my-ip",
+					"mapping_method": "claim",
+					"github": {
+						"client_id": "99999",
+						"organizations": [
+							"myorg"
+						]
+			***REMOVED***
+		***REMOVED***`***REMOVED***,
+			***REMOVED***,
+		***REMOVED***
+
+		terraform.Source(`
+			resource "rhcs_identity_provider" "my-ip" {
+				# (resource arguments***REMOVED***
+	***REMOVED***
+		`***REMOVED***
+
+		Expect(terraform.Import("rhcs_identity_provider.my-ip", "123,my-ip"***REMOVED******REMOVED***.To(BeZero(***REMOVED******REMOVED***
+		resource := terraform.Resource("rhcs_identity_provider", "my-ip"***REMOVED***
+		Expect(resource***REMOVED***.To(MatchJQ(".attributes.name", "my-ip"***REMOVED******REMOVED***
+		Expect(resource***REMOVED***.To(MatchJQ(".attributes.github.client_id", "99999"***REMOVED******REMOVED***
+	}***REMOVED***
+
+	It("Is an error if the identity provider isn't found", func(***REMOVED*** {
+		// Prepare the server:
+		server.AppendHandlers(
+			// List IDPs to map name to ID:
+			CombineHandlers(
+				VerifyRequest(
+					http.MethodGet,
+					"/api/clusters_mgmt/v1/clusters/123/identity_providers",
+				***REMOVED***,
+				RespondWithJSON(http.StatusOK, `{
+					"kind": "IdentityProviderList",
+					"href": "/api/clusters_mgmt/v1/clusters/24vg6o424djht8h6lpoli2urg69t7vnt/identity_providers",
+					"page": 1,
+					"size": 1,
+					"total": 1,
+					"items": [
+						{
+						"kind": "IdentityProvider",
+						"type": "GithubIdentityProvider",
+						"href": "/api/clusters_mgmt/v1/clusters/24vg6o424djht8h6lpoli2urg69t7vnt/identity_providers/24vgs9hgnl5bukujvkcmgkvfgc01ss0r",
+						"id": "24vgs9hgnl5bukujvkcmgkvfgc01ss0r",
+						"name": "my-ip",
+						"mapping_method": "claim",
+						"github": {
+							"client_id": "99999",
+							"organizations": [
+								"myorg"
+							]
+				***REMOVED***
+				***REMOVED***
+					]
+		***REMOVED***`***REMOVED***,
+			***REMOVED***,
+		***REMOVED***
+
+		terraform.Source(`
+			resource "rhcs_identity_provider" "my-ip" {
+				# (resource arguments***REMOVED***
+	***REMOVED***
+		`***REMOVED***
+
+		Expect(terraform.Import("rhcs_identity_provider.my-ip", "123,notfound"***REMOVED******REMOVED***.NotTo(BeZero(***REMOVED******REMOVED***
+	}***REMOVED***
+}***REMOVED***

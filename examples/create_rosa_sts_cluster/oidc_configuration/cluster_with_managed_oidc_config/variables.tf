9936c80e708a1b4c43984eb9d2d8121cf4f32292
Danielle Barda <dbarda@redhat.com>
2023-08-01 14:32:14 +0300

    Fix the default value of openshift_version in the examples

--- examples/create_rosa_sts_cluster/oidc_configuration/cluster_with_managed_oidc_config/variables.tf
+++ examples/create_rosa_sts_cluster/oidc_configuration/cluster_with_managed_oidc_config/variables.tf
@@ -48,7 +48,6 @@ variable "tags" {
 variable "openshift_version" {
   description = "Desired version of OpenShift for the cluster, for example '4.1.0'. If version is greater than the currently running version, an upgrade will be scheduled."
   type        = string
-  default     = "4.13.0"
 }
 
 variable "replicas" {

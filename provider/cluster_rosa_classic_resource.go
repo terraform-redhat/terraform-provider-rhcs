855e3bb820c799dbf6259f731da33a3a0eecac92
Sagi Dayan <sagidayan@gmail.com>
2023-09-03 17:31:54 +0300

    OCM-3124: fix for additional_trust_bundle resulting in an error
    Since OCM returns `REDUCTED` as values of sensitive content, the
    provider was populating `REDUCTED` as the new state for
    `additional_trust_bundle`. This commit ensures that if there is a value
    in the state, do not override value from backend.
    
    Signed-off-by: Sagi Dayan <sagidayan@gmail.com>

--- provider/cluster_rosa_classic_resource.go
+++ provider/cluster_rosa_classic_resource.go
@@ -1731,7 +1731,7 @@ func populateRosaClassicClusterState(ctx context.Context, object *cmv1.Cluster, 
 	}
 
 	trustBundle, ok := object.GetAdditionalTrustBundle()
-	if ok {
+	if ok && common.IsStringAttributeEmpty(state.Proxy.AdditionalTrustBundle) {
 		if state.Proxy == nil {
 			state.Proxy = &Proxy{}
 		}

e65a097da4d55bef45a34c4619a1e6e8622733da
John Strunk <jstrunk@redhat.com>
2023-09-08 16:46:12 -0400

    Checkpoint: cluster_rosa_classic_resource
    Signed-off-by: John Strunk <jstrunk@redhat.com>

--- provider/sts.go
+++ provider/sts.go
@@ -1,73 +1,65 @@
 package provider
 
 import (
-	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
-	"github.com/hashicorp/terraform-plugin-framework/types"
+	tfrschema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 )
 
-func stsResource() tfsdk.NestedAttributes {
-	return tfsdk.SingleNestedAttributes(map[string]tfsdk.Attribute{
-		"oidc_endpoint_url": {
+func stsResource() map[string]tfrschema.Attribute {
+	return map[string]tfrschema.Attribute{
+		"oidc_endpoint_url": tfrschema.StringAttribute{
 			Description: "OIDC Endpoint URL",
-			Type:        types.StringType,
 			Optional:    true,
 			Computed:    true,
-			PlanModifiers: []tfsdk.AttributePlanModifier{
+			PlanModifiers: []planmodifier.String{
 				// This passes the state through to the plan, so that the OIDC
 				// provider URL will not be "unknown" at plan-time, resulting in
 				// the oidc provider being needlessly replaced. This should be
 				// OK, since the OIDC provider URL is not expected to change.
-				tfsdk.UseStateForUnknown(),
+				stringplanmodifier.UseStateForUnknown(),
 			},
 		},
-		"oidc_config_id": {
+		"oidc_config_id": tfrschema.StringAttribute{
 			Description: "OIDC Configuration ID",
-			Type:        types.StringType,
 			Optional:    true,
 		},
-		"thumbprint": {
+		"thumbprint": tfrschema.StringAttribute{
 			Description: "SHA1-hash value of the root CA of the issuer URL",
-			Type:        types.StringType,
 			Computed:    true,
-			PlanModifiers: []tfsdk.AttributePlanModifier{
+			PlanModifiers: []planmodifier.String{
 				// This passes the state through to the plan, so that the OIDC
 				// thumbprint will not be "unknown" at plan-time, resulting in
 				// the oidc provider being needlessly replaced. This should be
 				// OK, since the thumbprint is not expected to change.
-				tfsdk.UseStateForUnknown(),
+				stringplanmodifier.UseStateForUnknown(),
 			},
 		},
-		"role_arn": {
+		"role_arn": tfrschema.StringAttribute{
 			Description: "Installer Role",
-			Type:        types.StringType,
 			Required:    true,
 		},
-		"support_role_arn": {
+		"support_role_arn": tfrschema.StringAttribute{
 			Description: "Support Role",
-			Type:        types.StringType,
 			Required:    true,
 		},
-		"instance_iam_roles": {
+		"instance_iam_roles": tfrschema.SingleNestedAttribute{
 			Description: "Instance IAM Roles",
-			Attributes: tfsdk.SingleNestedAttributes(map[string]tfsdk.Attribute{
-				"master_role_arn": {
+			Attributes: map[string]tfrschema.Attribute{
+				"master_role_arn": tfrschema.StringAttribute{
 					Description: "Master/Controller Plane Role ARN",
-					Type:        types.StringType,
 					Required:    true,
 				},
-				"worker_role_arn": {
+				"worker_role_arn": tfrschema.StringAttribute{
 					Description: "Worker Node Role ARN",
-					Type:        types.StringType,
 					Required:    true,
 				},
-			}),
+			},
 			Required: true,
 		},
-		"operator_role_prefix": {
+		"operator_role_prefix": tfrschema.StringAttribute{
 			Description: "Operator IAM Role prefix",
-			Type:        types.StringType,
 			Required:    true,
 		},
-	})
-
+	}
 }

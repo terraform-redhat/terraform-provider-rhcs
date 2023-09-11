c42f669f81ad315c3da6223d337f5b73ab3c08a5
John Strunk <jstrunk@redhat.com>
2023-09-11 11:42:18 -0400

    Remove import aliases tfdschema and tfrschema
    Signed-off-by: John Strunk <jstrunk@redhat.com>

--- provider/dnsdomain/dns_domain_resource.go
+++ provider/dnsdomain/dns_domain_resource.go
@@ -23,7 +23,7 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
-	tfrschema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	sdk "github.com/openshift-online/ocm-sdk-go"
@@ -47,10 +47,10 @@ func (r *DNSDomainResource) Metadata(ctx context.Context, req resource.MetadataR
 }
 
 func (r *DNSDomainResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
-	resp.Schema = tfrschema.Schema{
+	resp.Schema = schema.Schema{
 		Description: "DNS Domain",
-		Attributes: map[string]tfrschema.Attribute{
-			"id": tfrschema.StringAttribute{
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
 				Description: "Unique identifier of the DNS Domain",
 				Computed:    true,
 			},

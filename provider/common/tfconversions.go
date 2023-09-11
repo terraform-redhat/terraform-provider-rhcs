0bb153f47f9452b07b57127c29fc07e812a081e0
Danielle Barda <dbarda@redhat.com>
2023-09-11 17:04:47 +0300

    Update the conversions to be updated with the new types

--- provider/common/tfconversions.go
+++ provider/common/tfconversions.go
@@ -1,43 +1,89 @@
 package common
 
-import "github.com/hashicorp/terraform-plugin-framework/types"
+***REMOVED***
+	"context"
+***REMOVED***
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+***REMOVED***
 
 func OptionalInt64(tfVal types.Int64***REMOVED*** *int64 {
-	if tfVal.Unknown || tfVal.Null {
-		return nil
+	if !tfVal.IsNull(***REMOVED*** && !tfVal.IsUnknown(***REMOVED*** {
+		return pointer(tfVal.ValueInt64(***REMOVED******REMOVED***
 	}
-	return &tfVal.Value
+	return nil
 }
 
-func Bool(tfVal types.Bool***REMOVED*** bool {
-	return !tfVal.Unknown && !tfVal.Null && tfVal.Value
+func OptionalBool(tfVal types.Bool***REMOVED*** *bool {
+	if !tfVal.IsNull(***REMOVED*** && !tfVal.IsUnknown(***REMOVED*** {
+		return pointer(tfVal.ValueBool(***REMOVED******REMOVED***
+	}
+	return nil
+}
+
+func BoolWithFalseDefault(tfVal types.Bool***REMOVED*** bool {
+	if !tfVal.IsNull(***REMOVED*** && !tfVal.IsUnknown(***REMOVED*** {
+		return tfVal.ValueBool(***REMOVED***
+	}
+	return false
+}
+
+func pointer[T any](src T***REMOVED*** *T {
+	return &src
 }
 
 func OptionalString(tfVal types.String***REMOVED*** *string {
-	if tfVal.Unknown || tfVal.Null {
+	if tfVal.IsNull(***REMOVED*** || tfVal.IsUnknown(***REMOVED*** {
 		return nil
 	}
-	return &tfVal.Value
+	return pointer(tfVal.ValueString(***REMOVED******REMOVED***
 }
 
-func OptionalMap(tfVal types.Map***REMOVED*** map[string]string {
-	if tfVal.Unknown || tfVal.Null {
-		return nil
+func OptionalMap(ctx context.Context, tfVal types.Map***REMOVED*** (map[string]string, error***REMOVED*** {
+	if tfVal.IsNull(***REMOVED*** || tfVal.IsUnknown(***REMOVED*** {
+		return nil, nil
 	}
-	result := map[string]string{}
-	for k, v := range tfVal.Elems {
-		result[k] = v.(types.String***REMOVED***.Value
+	result := make(map[string]string, len(tfVal.Elements(***REMOVED******REMOVED******REMOVED***
+	d := tfVal.ElementsAs(ctx, &result, false***REMOVED***
+	if d.HasError(***REMOVED*** {
+		return nil, fmt.Errorf("error converting to map object %v", d.Errors(***REMOVED***[0].Detail(***REMOVED******REMOVED***
 	}
-	return result
+
+	return result, nil
 }
 
-func OptionalList(tfVal types.List***REMOVED*** []string {
-	if tfVal.Unknown || tfVal.Null {
-		return nil
+func OptionalList(ctx context.Context, tfVal types.List***REMOVED*** ([]string, error***REMOVED*** {
+	if tfVal.IsNull(***REMOVED*** || tfVal.IsUnknown(***REMOVED*** {
+		return nil, nil
 	}
-	result := make([]string, 0***REMOVED***
-	for _, e := range tfVal.Elems {
-		result = append(result, e.(types.String***REMOVED***.Value***REMOVED***
+	result := make([]string, len(tfVal.Elements(***REMOVED******REMOVED******REMOVED***
+	d := tfVal.ElementsAs(ctx, &result, false***REMOVED***
+	if d.HasError(***REMOVED*** {
+		return nil, fmt.Errorf("error converting to map object %v", d.Errors(***REMOVED***[0].Detail(***REMOVED******REMOVED***
 	}
-	return result
+	return result, nil
+}
+
+func ConvertStringMapToMapType(stringMap map[string]string***REMOVED*** (types.Map, error***REMOVED*** {
+	elements := map[string]attr.Value{}
+	for k, v := range stringMap {
+		elements[k] = types.StringValue(v***REMOVED***
+	}
+	mapValue, diags := types.MapValue(types.StringType, elements***REMOVED***
+	if diags != nil && diags.HasError(***REMOVED*** {
+		fmt.Errorf("failed to convert to MapType %v", diags.Errors(***REMOVED***[0].Detail(***REMOVED******REMOVED***
+	}
+	return mapValue, nil
+}
+
+func ConvertStringListToListType(stringList []string***REMOVED*** (types.List, error***REMOVED*** {
+	elements := []attr.Value{}
+	for _, e := range stringList {
+		elements = append(elements, types.StringValue(e***REMOVED******REMOVED***
+	}
+	listValue, diags := types.ListValue(types.StringType, elements***REMOVED***
+	if diags != nil && diags.HasError(***REMOVED*** {
+		fmt.Errorf("failed to convert to List type %v", diags.Errors(***REMOVED***[0].Detail(***REMOVED******REMOVED***
+	}
+	return listValue, nil
 }

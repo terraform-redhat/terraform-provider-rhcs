b6cf59690eae568de9e95f12c77eabceb275d2a3
Danielle Barda <dbarda@redhat.com>
2023-09-11 17:04:47 +0300

    Update the conversions to be updated with the new types

--- provider/common/tfconversions.go
+++ provider/common/tfconversions.go
@@ -1,43 +1,89 @@
 package common
 
-import "github.com/hashicorp/terraform-plugin-framework/types"
+import (
+	"context"
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
 
 func OptionalInt64(tfVal types.Int64) *int64 {
-	if tfVal.Unknown || tfVal.Null {
-		return nil
+	if !tfVal.IsNull() && !tfVal.IsUnknown() {
+		return pointer(tfVal.ValueInt64())
 	}
-	return &tfVal.Value
+	return nil
 }
 
-func Bool(tfVal types.Bool) bool {
-	return !tfVal.Unknown && !tfVal.Null && tfVal.Value
+func OptionalBool(tfVal types.Bool) *bool {
+	if !tfVal.IsNull() && !tfVal.IsUnknown() {
+		return pointer(tfVal.ValueBool())
+	}
+	return nil
+}
+
+func BoolWithFalseDefault(tfVal types.Bool) bool {
+	if !tfVal.IsNull() && !tfVal.IsUnknown() {
+		return tfVal.ValueBool()
+	}
+	return false
+}
+
+func pointer[T any](src T) *T {
+	return &src
 }
 
 func OptionalString(tfVal types.String) *string {
-	if tfVal.Unknown || tfVal.Null {
+	if tfVal.IsNull() || tfVal.IsUnknown() {
 		return nil
 	}
-	return &tfVal.Value
+	return pointer(tfVal.ValueString())
 }
 
-func OptionalMap(tfVal types.Map) map[string]string {
-	if tfVal.Unknown || tfVal.Null {
-		return nil
+func OptionalMap(ctx context.Context, tfVal types.Map) (map[string]string, error) {
+	if tfVal.IsNull() || tfVal.IsUnknown() {
+		return nil, nil
 	}
-	result := map[string]string{}
-	for k, v := range tfVal.Elems {
-		result[k] = v.(types.String).Value
+	result := make(map[string]string, len(tfVal.Elements()))
+	d := tfVal.ElementsAs(ctx, &result, false)
+	if d.HasError() {
+		return nil, fmt.Errorf("error converting to map object %v", d.Errors()[0].Detail())
 	}
-	return result
+
+	return result, nil
 }
 
-func OptionalList(tfVal types.List) []string {
-	if tfVal.Unknown || tfVal.Null {
-		return nil
+func OptionalList(ctx context.Context, tfVal types.List) ([]string, error) {
+	if tfVal.IsNull() || tfVal.IsUnknown() {
+		return nil, nil
 	}
-	result := make([]string, 0)
-	for _, e := range tfVal.Elems {
-		result = append(result, e.(types.String).Value)
+	result := make([]string, len(tfVal.Elements()))
+	d := tfVal.ElementsAs(ctx, &result, false)
+	if d.HasError() {
+		return nil, fmt.Errorf("error converting to map object %v", d.Errors()[0].Detail())
 	}
-	return result
+	return result, nil
+}
+
+func ConvertStringMapToMapType(stringMap map[string]string) (types.Map, error) {
+	elements := map[string]attr.Value{}
+	for k, v := range stringMap {
+		elements[k] = types.StringValue(v)
+	}
+	mapValue, diags := types.MapValue(types.StringType, elements)
+	if diags != nil && diags.HasError() {
+		fmt.Errorf("failed to convert to MapType %v", diags.Errors()[0].Detail())
+	}
+	return mapValue, nil
+}
+
+func ConvertStringListToListType(stringList []string) (types.List, error) {
+	elements := []attr.Value{}
+	for _, e := range stringList {
+		elements = append(elements, types.StringValue(e))
+	}
+	listValue, diags := types.ListValue(types.StringType, elements)
+	if diags != nil && diags.HasError() {
+		fmt.Errorf("failed to convert to List type %v", diags.Errors()[0].Detail())
+	}
+	return listValue, nil
 }

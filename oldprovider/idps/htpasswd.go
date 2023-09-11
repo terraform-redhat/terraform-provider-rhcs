055a240ede5324016d52a6fc7254d1bc321f2b80
Danielle Barda <dbarda@redhat.com>
2023-09-11 22:40:20 +0300

    Moving common vlidations into common package

--- oldprovider/idps/htpasswd.go
+++ oldprovider/idps/htpasswd.go
@@ -3,9 +3,6 @@ package idps
 import (
 	"context"
 	"fmt"
-	"regexp"
-	"strings"
-
 	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	cmv1 "github.com/openshift-online/ocm-sdk-go/clustersmgmt/v1"
@@ -105,31 +102,4 @@ func HTPasswdValidators() []tfsdk.AttributeValidator {
 			},
 		},
 	}
-}
-
-func ValidateHTPasswdUsername(username string) error {
-	if strings.ContainsAny(username, "/:%") {
-		return fmt.Errorf("invalid username '%s': "+
-			"username must not contain /, :, or %%", username)
-	}
-	return nil
-}
-
-func ValidateHTPasswdPassword(password string) error {
-	notAsciiOnly, _ := regexp.MatchString(`[^\x20-\x7E]`, password)
-	containsSpace := strings.Contains(password, " ")
-	tooShort := len(password) < 14
-	if notAsciiOnly || containsSpace || tooShort {
-		return fmt.Errorf(
-			"password must be at least 14 characters (ASCII-standard) without whitespaces")
-	}
-	hasUppercase, _ := regexp.MatchString(`[A-Z]`, password)
-	hasLowercase, _ := regexp.MatchString(`[a-z]`, password)
-	hasNumberOrSymbol, _ := regexp.MatchString(`[^a-zA-Z]`, password)
-	if !hasUppercase || !hasLowercase || !hasNumberOrSymbol {
-		return fmt.Errorf(
-			"password must include uppercase letters, lowercase letters, and numbers " +
-				"or symbols (ASCII-standard characters only)")
-	}
-	return nil
 }

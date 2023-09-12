3fcc790297f5d7ed385162cb3d57bb4798442e6a
Igal Tsoiref <itsoiref@redhat.com>
2023-09-12 12:07:25 +0300

    Moving oidc_config to new framework

--- provider/oidcconfig/rosa_oidc_config_resource.go
+++ provider/oidcconfig/rosa_oidc_config_resource.go
@@ -22,61 +22,64 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/hashicorp/terraform-plugin-framework/diag"
-	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/types"
-	"github.com/hashicorp/terraform-plugin-go/tftypes"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
+	sdk "github.com/openshift-online/ocm-sdk-go"
 	cmv1 "github.com/openshift-online/ocm-sdk-go/clustersmgmt/v1"
-)
 
-type RosaOidcConfigResourceType struct {
-}
+	"github.com/terraform-redhat/terraform-provider-rhcs/provider/common"
+)
 
 type RosaOidcConfigResource struct {
 	oidcConfigClient *cmv1.OidcConfigsClient
 	clustersClient   *cmv1.ClustersClient
 }
 
-func (t *RosaOidcConfigResourceType) GetSchema(ctx context.Context) (result tfsdk.Schema,
-	diags diag.Diagnostics) {
-	result = tfsdk.Schema{
-		Description: "OIDC config",
-		Attributes: map[string]tfsdk.Attribute{
-			"managed": {
+var _ resource.ResourceWithConfigure = &RosaOidcConfigResource{}
+var _ resource.ResourceWithImportState = &RosaOidcConfigResource{}
+
+func New() resource.Resource {
+	return &RosaOidcConfigResource{}
+}
+
+func (o *RosaOidcConfigResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_rosa_oidc_config"
+}
+
+func (o *RosaOidcConfigResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Description: "Manages OIDC config",
+		Attributes: map[string]schema.Attribute{
+			"managed": schema.BoolAttribute{
 				Description: "Indicates whether it is a Red Hat managed or unmanaged (Customer hosted) OIDC Configuration",
-				Type:        types.BoolType,
 				Required:    true,
 			},
-			"secret_arn": {
+			"secret_arn": schema.StringAttribute{
 				Description: "Indicates for unmanaged OIDC config, the secret ARN",
-				Type:        types.StringType,
 				Optional:    true,
 			},
-			"issuer_url": {
+			"issuer_url": schema.StringAttribute{
 				Description: "The bucket URL",
-				Type:        types.StringType,
 				Optional:    true,
 				Computed:    true,
 			},
-			"installer_role_arn": {
+			"installer_role_arn": schema.StringAttribute{
 				Description: "STS Role ARN with get secrets permission",
-				Type:        types.StringType,
 				Optional:    true,
 			},
-			"id": {
+			"id": schema.StringAttribute{
 				Description: "The OIDC config ID",
-				Type:        types.StringType,
 				Computed:    true,
 			},
-			"thumbprint": {
+			"thumbprint": schema.StringAttribute{
 				Description: "SHA1-hash value of the root CA of the issuer URL",
-				Type:        types.StringType,
 				Computed:    true,
 			},
-			"oidc_endpoint_url": {
+			"oidc_endpoint_url": schema.StringAttribute{
 				Description: "OIDC Endpoint URL",
-				Type:        types.StringType,
 				Computed:    true,
 			},
 		},
@@ -84,27 +87,27 @@ func (t *RosaOidcConfigResourceType) GetSchema(ctx context.Context) (result tfsd
 	return
 }
 
-func (t *RosaOidcConfigResourceType) NewResource(ctx context.Context,
-	p tfsdk.Provider) (result tfsdk.Resource, diags diag.Diagnostics) {
-	// Cast the provider interface to the specific implementation: use it directly when needed.
-	parent := p.(*Provider)
-
-	// Get the oidcConfigClient:
-	oidcConfigClient := parent.connection.ClustersMgmt().V1().OidcConfigs()
-	// Get the clustersClient:
-	clustersClient := parent.connection.ClustersMgmt().V1().Clusters()
-
-	// Create the resource:
-	result = &RosaOidcConfigResource{
-		oidcConfigClient: oidcConfigClient,
-		clustersClient:   clustersClient,
+func (o *RosaOidcConfigResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	// Prevent panic if the provider has not been configured.
+	if req.ProviderData == nil {
+		return
 	}
 
-	return
+	connection, ok := req.ProviderData.(*sdk.Connection)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Resource Configure Type",
+			fmt.Sprintf("Expected *sdk.Connaction, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+		return
+	}
+
+	o.oidcConfigClient = connection.ClustersMgmt().V1().OidcConfigs()
+	o.clustersClient = connection.ClustersMgmt().V1().Clusters()
 }
 
-func (r *RosaOidcConfigResource) Create(ctx context.Context,
-	request tfsdk.CreateResourceRequest, response *tfsdk.CreateResourceResponse) {
+func (o *RosaOidcConfigResource) Create(ctx context.Context, request resource.CreateRequest,
+	response *resource.CreateResponse) {
 	// Get the plan:
 	state := &RosaOidcConfigState{}
 	diags := request.Plan.Get(ctx, state)
@@ -113,13 +116,13 @@ func (r *RosaOidcConfigResource) Create(ctx context.Context,
 		return
 	}
 
-	managed := state.Managed.Value
+	managed := state.Managed.ValueBool()
 	var oidcConfig *cmv1.OidcConfig
 	var err error
 	if managed {
-		if (!state.SecretARN.Unknown && !state.SecretARN.Null) ||
-			(!state.IssuerUrl.Unknown && !state.IssuerUrl.Null) ||
-			(!state.InstallerRoleARN.Unknown && !state.InstallerRoleARN.Null) {
+		if (!state.SecretARN.IsUnknown() && !state.SecretARN.IsNull()) ||
+			(!state.IssuerUrl.IsUnknown() && !state.IssuerUrl.IsNull()) ||
+			(!state.InstallerRoleARN.IsUnknown() && !state.InstallerRoleARN.IsNull()) {
 			response.Diagnostics.AddError(
 				"Attribute's values are not supported for managed OIDC Configuration",
 				fmt.Sprintf(
@@ -140,9 +143,9 @@ func (r *RosaOidcConfigResource) Create(ctx context.Context,
 			return
 		}
 	} else {
-		if state.SecretARN.Unknown || state.SecretARN.Null ||
-			state.IssuerUrl.Unknown || state.IssuerUrl.Null ||
-			state.InstallerRoleARN.Unknown || state.InstallerRoleARN.Null {
+		if state.SecretARN.IsUnknown() || state.SecretARN.IsNull() ||
+			state.IssuerUrl.IsUnknown() || state.IssuerUrl.IsNull() ||
+			state.InstallerRoleARN.IsUnknown() || state.InstallerRoleARN.IsNull() {
 			response.Diagnostics.AddError(
 				"There is a missing parameter for unmanaged OIDC Configuration",
 				fmt.Sprintf(
@@ -154,9 +157,9 @@ func (r *RosaOidcConfigResource) Create(ctx context.Context,
 		}
 		oidcConfig, err = cmv1.NewOidcConfig().
 			Managed(false).
-			SecretArn(state.SecretARN.Value).
-			IssuerUrl(state.IssuerUrl.Value).
-			InstallerRoleArn(state.InstallerRoleARN.Value).
+			SecretArn(state.SecretARN.ValueString()).
+			IssuerUrl(state.IssuerUrl.ValueString()).
+			InstallerRoleArn(state.InstallerRoleARN.ValueString()).
 			Build()
 
 		if err != nil {
@@ -170,7 +173,7 @@ func (r *RosaOidcConfigResource) Create(ctx context.Context,
 		}
 	}
 
-	object, err := r.oidcConfigClient.Add().Body(oidcConfig).SendContext(ctx)
+	object, err := o.oidcConfigClient.Add().Body(oidcConfig).SendContext(ctx)
 	if err != nil {
 		response.Diagnostics.AddError(
 			"There was a problem registering the OIDC Configuration",
@@ -184,13 +187,12 @@ func (r *RosaOidcConfigResource) Create(ctx context.Context,
 	oidcConfig = object.Body()
 
 	// Save the state:
-	r.populateState(ctx, oidcConfig, state)
+	o.populateState(ctx, oidcConfig, state)
 	diags = response.State.Set(ctx, state)
 	response.Diagnostics.Append(diags...)
 }
 
-func (r *RosaOidcConfigResource) Read(ctx context.Context, request tfsdk.ReadResourceRequest,
-	response *tfsdk.ReadResourceResponse) {
+func (o *RosaOidcConfigResource) Read(ctx context.Context, request resource.ReadRequest, response *resource.ReadResponse) {
 	// Get the current state:
 	state := &RosaOidcConfigState{}
 	diags := request.State.Get(ctx, state)
@@ -200,10 +202,10 @@ func (r *RosaOidcConfigResource) Read(ctx context.Context, request tfsdk.ReadRes
 	}
 
 	// Find the oidc config:
-	get, err := r.oidcConfigClient.OidcConfig(state.ID.Value).Get().SendContext(ctx)
+	get, err := o.oidcConfigClient.OidcConfig(state.ID.ValueString()).Get().SendContext(ctx)
 	if err != nil && get.Status() == http.StatusNotFound {
 		tflog.Warn(ctx, fmt.Sprintf("oidc config (%s) not found, removing from state",
-			state.ID.Value,
+			state.ID.ValueString(),
 		))
 		response.State.RemoveResource(ctx)
 		return
@@ -212,7 +214,7 @@ func (r *RosaOidcConfigResource) Read(ctx context.Context, request tfsdk.ReadRes
 			"Can't find OIDC config",
 			fmt.Sprintf(
 				"Can't find OIDC config with ID %s, %v",
-				state.ID.Value, err,
+				state.ID.ValueString(), err,
 			),
 		)
 		return
@@ -221,13 +223,13 @@ func (r *RosaOidcConfigResource) Read(ctx context.Context, request tfsdk.ReadRes
 	object := get.Body()
 
 	// Save the state:
-	r.populateState(ctx, object, state)
+	o.populateState(ctx, object, state)
 	diags = response.State.Set(ctx, state)
 	response.Diagnostics.Append(diags...)
 }
 
-func (r *RosaOidcConfigResource) Update(ctx context.Context, request tfsdk.UpdateResourceRequest,
-	response *tfsdk.UpdateResourceResponse) {
+func (o *RosaOidcConfigResource) Update(ctx context.Context, request resource.UpdateRequest,
+	response *resource.UpdateResponse) {
 	response.Diagnostics.AddError(
 		"Update methode is not supported for that resource",
 		fmt.Sprintf(
@@ -237,8 +239,7 @@ func (r *RosaOidcConfigResource) Update(ctx context.Context, request tfsdk.Updat
 	return
 }
 
-func (r *RosaOidcConfigResource) Delete(ctx context.Context, request tfsdk.DeleteResourceRequest,
-	response *tfsdk.DeleteResourceResponse) {
+func (o *RosaOidcConfigResource) Delete(ctx context.Context, request resource.DeleteRequest, response *resource.DeleteResponse) {
 	// Get the state:
 	state := &RosaOidcConfigState{}
 	diags := request.State.Get(ctx, state)
@@ -248,13 +249,13 @@ func (r *RosaOidcConfigResource) Delete(ctx context.Context, request tfsdk.Delet
 	}
 
 	// Find the oidc config:
-	get, err := r.oidcConfigClient.OidcConfig(state.ID.Value).Get().SendContext(ctx)
+	get, err := o.oidcConfigClient.OidcConfig(state.ID.ValueString()).Get().SendContext(ctx)
 	if err != nil {
 		response.Diagnostics.AddError(
 			"Can't find OIDC config",
 			fmt.Sprintf(
 				"Can't find OIDC config with ID %s, %v",
-				state.ID.Value, err,
+				state.ID.ValueString(), err,
 			),
 		)
 		return
@@ -263,7 +264,7 @@ func (r *RosaOidcConfigResource) Delete(ctx context.Context, request tfsdk.Delet
 	oidcConfig := get.Body()
 
 	// check if there is a cluster using the oidc endpoint:
-	hasClusterUsingOidcConfig, err := r.hasAClusterUsingOidcEndpointUrl(ctx, oidcConfig.IssuerUrl())
+	hasClusterUsingOidcConfig, err := o.hasAClusterUsingOidcEndpointUrl(ctx, oidcConfig.IssuerUrl())
 	if err != nil {
 		response.Diagnostics.AddError(
 			"There was a problem checking if any clusters are using OIDC config",
@@ -285,7 +286,7 @@ func (r *RosaOidcConfigResource) Delete(ctx context.Context, request tfsdk.Delet
 		return
 	}
 
-	err = r.deleteOidcConfig(ctx, state.ID.Value)
+	err = o.deleteOidcConfig(ctx, state.ID.ValueString())
 	if err != nil {
 		response.Diagnostics.AddError(
 			"There was a problem deleting the OIDC config",
@@ -301,11 +302,11 @@ func (r *RosaOidcConfigResource) Delete(ctx context.Context, request tfsdk.Delet
 	response.State.RemoveResource(ctx)
 }
 
-func (r *RosaOidcConfigResource) hasAClusterUsingOidcEndpointUrl(ctx context.Context, issuerUrl string) (bool, error) {
+func (o *RosaOidcConfigResource) hasAClusterUsingOidcEndpointUrl(ctx context.Context, issuerUrl string) (bool, error) {
 	query := fmt.Sprintf(
 		"aws.sts.oidc_endpoint_url = '%s'", issuerUrl,
 	)
-	request := r.clustersClient.List().Search(query)
+	request := o.clustersClient.List().Search(query)
 	page := 1
 	response, err := request.Page(page).SendContext(ctx)
 	if err != nil {
@@ -317,71 +318,61 @@ func (r *RosaOidcConfigResource) hasAClusterUsingOidcEndpointUrl(ctx context.Con
 	return false, nil
 }
 
-func (r *RosaOidcConfigResource) deleteOidcConfig(ctx context.Context, id string) error {
-	_, err := r.oidcConfigClient.
+func (o *RosaOidcConfigResource) deleteOidcConfig(ctx context.Context, id string) error {
+	_, err := o.oidcConfigClient.
 		OidcConfig(id).
 		Delete().
 		SendContext(ctx)
 	return err
 }
 
-func (r *RosaOidcConfigResource) ImportState(ctx context.Context, request tfsdk.ImportResourceStateRequest,
-	response *tfsdk.ImportResourceStateResponse) {
-	tfsdk.ResourceImportStatePassthroughID(
+func (o *RosaOidcConfigResource) ImportState(ctx context.Context, request resource.ImportStateRequest,
+	response *resource.ImportStateResponse) {
+	resource.ImportStatePassthroughID(
 		ctx,
-		tftypes.NewAttributePath().WithAttributeName("id"),
+		path.Root("id"),
 		request,
 		response,
 	)
 }
 
 // populateState copies the data from the API object to the Terraform state.
-func (r *RosaOidcConfigResource) populateState(ctx context.Context, object *cmv1.OidcConfig, state *RosaOidcConfigState) {
-	state.ID = types.String{
-		Value: object.ID(),
+func (o *RosaOidcConfigResource) populateState(ctx context.Context, object *cmv1.OidcConfig, state *RosaOidcConfigState) {
+	if id, ok := object.GetID(); ok {
+		state.ID = types.StringValue(id)
 	}
-	state.Managed = types.Bool{
-		Value: object.Managed(),
+
+	if managed, ok := object.GetManaged(); ok {
+		state.Managed = types.BoolValue(managed)
 	}
 
 	issuerUrl, ok := object.GetIssuerUrl()
 	if ok && issuerUrl != "" {
-		state.IssuerUrl = types.String{
-			Value: issuerUrl,
-		}
+		state.IssuerUrl = types.StringValue(issuerUrl)
 	}
 
 	installerRoleArn, ok := object.GetInstallerRoleArn()
 	if ok && installerRoleArn != "" {
-		state.InstallerRoleARN = types.String{
-			Value: installerRoleArn,
-		}
+		state.InstallerRoleARN = types.StringValue(installerRoleArn)
 	}
 
 	secretArn, ok := object.GetSecretArn()
 	if ok && secretArn != "" {
-		state.SecretARN = types.String{
-			Value: secretArn,
-		}
+		state.SecretARN = types.StringValue(secretArn)
 	}
+
 	oidcEndpointURL := issuerUrl
 	if strings.HasPrefix(oidcEndpointURL, "https://") {
 		oidcEndpointURL = strings.TrimPrefix(oidcEndpointURL, "https://")
 	}
-	state.OIDCEndpointURL = types.String{
-		Value: oidcEndpointURL,
-	}
+	state.OIDCEndpointURL = types.StringValue(oidcEndpointURL)
 
-	thumbprint, err := getThumbprint(issuerUrl, DefaultHttpClient{})
+	thumbprint, err := common.GetThumbprint(issuerUrl, common.DefaultHttpClient{})
 	if err != nil {
 		tflog.Error(ctx, fmt.Sprintf("cannot get thumbprint, with error: %v", err))
-		state.Thumbprint = types.String{
-			Value: "",
-		}
+		state.Thumbprint = types.StringValue("")
 	} else {
-		state.Thumbprint = types.String{
-			Value: thumbprint,
-		}
+		state.Thumbprint = types.StringValue(thumbprint)
 	}
 
 }

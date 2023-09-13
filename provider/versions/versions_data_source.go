03d878082deb6b36651cb56206753cacd1a04baa
Igal Tsoiref <itsoiref@redhat.com>
2023-09-13 12:23:29 +0300

    Typo fix for VersionsDataSource object

--- provider/versions/versions_data_source.go
+++ provider/versions/versions_data_source.go
@@ -69,7 +69,7 @@ func (s *VersionsDataSource) Schema(ctx context.Context, req datasource.SchemaRe
 	}
 }
 
-func (t *VersionsDataSource) itemAttributes() map[string]schema.Attribute {
+func (s *VersionsDataSource) itemAttributes() map[string]schema.Attribute {
 	return map[string]schema.Attribute{
 		"id": schema.StringAttribute{
 			Description: "Unique identifier of the version. This is what should be " +

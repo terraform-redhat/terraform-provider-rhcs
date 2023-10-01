441d0b173eaec0c48c641b59b54301cede9335e3
Sagi Dayan <sagidayan@gmail.com>
2023-10-01 18:43:30 +0300

    Moved some validations to new cluster validators.go file
    Using the common functions to generate the validators.
    Removed unneeded files.
    
    Signed-off-by: Sagi Dayan <sagidayan@gmail.com>

--- provider/clusterrosaclassic/validators.go
+++ provider/clusterrosaclassic/validators.go
@@ -7,7 +7,9 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
+	"github.com/terraform-redhat/terraform-provider-rhcs/provider/common"
 	"github.com/terraform-redhat/terraform-provider-rhcs/provider/common/attrvalidators"
 )
 
@@ -26,3 +28,47 @@ var availabilityZoneValidator = attrvalidators.NewStringValidator("", func(ctx c
 		return
 	}
 })
+
+var privateHZValidator = attrvalidators.NewObjectValidator("proxy map should not include an hard coded OCM proxy",
+	func(ctx context.Context, req validator.ObjectRequest, resp *validator.ObjectResponse) {
+		if req.ConfigValue.IsNull() || req.ConfigValue.IsUnknown() {
+			return
+		}
+
+		privateHZ := PrivateHostedZone{}
+		d := req.ConfigValue.As(ctx, &privateHZ, basetypes.ObjectAsOptions{})
+		if d.HasError() {
+			// No attribute to validate
+			return
+		}
+		errSum := "Invalid private_hosted_zone attribute assignment"
+
+		// validate ID and ARN are not empty
+		if common.IsStringAttributeEmpty(privateHZ.ID) || common.IsStringAttributeEmpty(privateHZ.RoleARN) {
+			resp.Diagnostics.AddError(errSum, "Invalid configuration. 'private_hosted_zone.id' and 'private_hosted_zone.arn' are required")
+			return
+		}
+
+	})
+
+var propertiesValidator = attrvalidators.NewMapValidator("properties map should not include an hard coded OCM properties",
+	func(ctx context.Context, req validator.MapRequest, resp *validator.MapResponse) {
+		if req.ConfigValue.IsNull() || req.ConfigValue.IsUnknown() {
+			return
+		}
+		propertiesElements := make(map[string]types.String, len(req.ConfigValue.Elements()))
+		d := req.ConfigValue.ElementsAs(ctx, &propertiesElements, false)
+		if d.HasError() {
+			// No attribute to validate
+			return
+		}
+
+		for k, _ := range propertiesElements {
+			if _, isDefaultKey := OCMProperties[k]; isDefaultKey {
+				errHead := "Invalid property key."
+				errDesc := fmt.Sprintf("Can not override reserved properties keys. %s is a reserved property key", k)
+				resp.Diagnostics.AddError(errHead, errDesc)
+				return
+			}
+		}
+	})

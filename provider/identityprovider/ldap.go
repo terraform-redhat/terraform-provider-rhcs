284f76d361bc5b1157be3d426aa39d9a6dab89d9
James Land <hokie10@gmail.com>
2023-10-11 15:59:19 -0400

    Removing unused import

--- provider/identityprovider/ldap.go
+++ provider/identityprovider/ldap.go
@@ -3,7 +3,6 @@ package identityprovider
 ***REMOVED***
 	"context"
 
-	"github.com/hashicorp/terraform-plugin-framework-validators/listvalidator"
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"

91bc539768d10a710f96f1c5928e3c8313af9daf
Aleksandra Malykhin <amalykhi@redhat.com>
2023-11-06 15:11:02 +0200

    f

--- go.mod
+++ go.mod
@@ -11,8 +11,8 @@ require (
 	github.com/hashicorp/terraform-plugin-framework-validators v0.12.0
 	github.com/hashicorp/terraform-plugin-log v0.9.0
 	github.com/hashicorp/terraform-plugin-sdk v1.17.2
-	github.com/onsi/ginkgo/v2 v2.11.0
-	github.com/onsi/gomega v1.27.10
+	github.com/onsi/ginkgo/v2 v2.13.0
+	github.com/onsi/gomega v1.28.1
 	github.com/openshift-online/ocm-common v0.0.0-20230724120046-183b85503854
 	github.com/openshift-online/ocm-sdk-go v0.1.362
 	github.com/openshift/rosa v1.2.21
@@ -47,7 +47,7 @@ require (
 	github.com/golang-jwt/jwt/v4 v4.4.3 // indirect
 	github.com/golang/glog v1.1.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
-	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/pprof v0.0.0-20210407192527-94a9f03dee38 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/css v1.0.0 // indirect
@@ -106,13 +106,13 @@ require (
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	github.com/zclconf/go-cty v1.13.2 // indirect
 	gitlab.com/c0b/go-ordered-json v0.0.0-20171130231205-49bbdab258c2 // indirect
-	golang.org/x/crypto v0.11.0 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
 	golang.org/x/exp v0.0.0-20230626212559-97b1e661b5df // indirect
-	golang.org/x/mod v0.11.0 // indirect
-	golang.org/x/net v0.13.0 // indirect
-	golang.org/x/sys v0.10.0 // indirect
-	golang.org/x/text v0.11.0 // indirect
-	golang.org/x/tools v0.9.3 // indirect
+	golang.org/x/mod v0.12.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/tools v0.12.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230525234030-28d5490b6b19 // indirect
 	google.golang.org/grpc v1.57.0 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
--- tests/e2e/machine_pool_test.go
+++ tests/e2e/machine_pool_test.go
@@ -53,9 +53,9 @@ var _ = Describe("TF Test", func() {
 					MachineType: machineType,
 					Name:        name,
 				}
-
 				err := mpService.Create(MachinePoolArgs)
 				Expect(err).ToNot(HaveOccurred())
+
 				By("Verify the parameters of the created machinepool")
 				mpOut, err := mpService.Output()
 				Expect(err).ToNot(HaveOccurred())
@@ -86,10 +86,55 @@ var _ = Describe("TF Test", func() {
 
 				err := mpService.Create(MachinePoolArgs)
 				Expect(err).ToNot(HaveOccurred())
+
 				By("Verify the parameters of the created machinepool")
 				mpResponseBody, err := cms.RetrieveClusterMachinePool(ci.RHCSConnection, clusterID, name)
 				Expect(err).ToNot(HaveOccurred())
 				Expect(mpResponseBody.Labels()).To(Equal(creationLabels))
+
+				By("Edit the labels of the machinepool")
+				MachinePoolArgs.Labels = updatingLabels
+				err = mpService.Create(MachinePoolArgs)
+				Expect(err).ToNot(HaveOccurred())
+				mpResponseBody, err = cms.RetrieveClusterMachinePool(ci.RHCSConnection, clusterID, name)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(mpResponseBody.Labels()).To(Equal(updatingLabels))
+
+				By("Delete the labels of the machinepool")
+				MachinePoolArgs.Labels = emptyLabels
+				err = mpService.Create(MachinePoolArgs)
+				Expect(err).ToNot(HaveOccurred())
+				mpResponseBody, err = cms.RetrieveClusterMachinePool(ci.RHCSConnection, clusterID, name)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(mpResponseBody.Labels()).To(BeNil())
+
+			})
+		})
+		Context("Author:amalykhi-High-OCP-64905 @OCP-64905 @amalykhi", func() {
+			It("Author:amalykhi-High-OCP-64905 Edit/delete second machinepool labels", ci.Day2, ci.High, ci.FeatureMachinepool, func() {
+				By("Create additional machinepool with labels")
+				replicas := 3
+				machineType := "r5.xlarge"
+				name := "ocp-64905"
+				creationLabels := map[string]string{"fo1": "bar1", "fo2": "baz2"}
+				updatingLabels := map[string]string{"fo1": "bar3", "fo3": "baz3"}
+				emptyLabels := map[string]string{}
+				MachinePoolArgs := &exe.MachinePoolArgs{
+					Token:       token,
+					Cluster:     clusterID,
+					Replicas:    replicas,
+					MachineType: machineType,
+					Name:        name,
+					Labels:      creationLabels,
+				}
+				err := mpService.Create(MachinePoolArgs)
+				Expect(err).ToNot(HaveOccurred())
+
+				By("Verify the parameters of the created machinepool")
+				mpResponseBody, err := cms.RetrieveClusterMachinePool(ci.RHCSConnection, clusterID, name)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(mpResponseBody.Labels()).To(Equal(creationLabels))
+
 				By("Edit the labels of the machinepool")
 				MachinePoolArgs.Labels = updatingLabels
 				err = mpService.Create(MachinePoolArgs)

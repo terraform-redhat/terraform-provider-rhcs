981db48ba8b858a3a10f116ff76a0b606c22aae2
smiron <smiron@redhat.com>
2023-12-27 14:08:23 +0200

    automate ocp-64907

--- tests/utils/constants/profile_defaults.go
+++ tests/utils/constants/profile_defaults.go
@@ -11,7 +11,7 @@ var (
 	DefaultMPLabels  = map[string]string{
 		"test1": "testdata1",
 	}
-	CustomProperties = map[string]string{"custom_property": "test"}
+	CustomProperties = map[string]string{"custom_property": "test", "qe_usage": RHCS.QEUsage}
 	LdapURL          = "ldap://ldap.forumsys.com/dc=example,dc=com?uid"
 	GitLabURL        = "https://gitlab.cee.redhat.com"
 	Organizations    = []string{"openshift"}

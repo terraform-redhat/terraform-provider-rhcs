6dc1d13b7d092f200e18fba1766c5b2e7ca8e574
Aleksandra Malykhin <amalykhi@redhat.com>
2024-01-11 13:28:59 +0200

    limit wait operators check to periodic jobs WAIT_OPERATOR var

--- tests/utils/constants/constants.go
+++ tests/utils/constants/constants.go
@@ -27,6 +27,7 @@ const (
 	MajorVersion              = "MAJOR_VERSION_ENV"
 	RHCSVersion               = "RHCS_VERSION"
 	RHCSSource                = "RHCS_SOURCE"
+	WaitOperators             = "WAIT_OPERATORS"
 )
 
 var (

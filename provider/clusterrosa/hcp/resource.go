126cda3a65fe9cf4b00f160a191cac2451012699
Guilherme Branco <gbranco@redhat.com>
2024-03-18 17:09:22 -0300

    OCM-6805 | fix: allow patch of billing account id on hcp cluster

--- provider/clusterrosa/hcp/resource.go
+++ provider/clusterrosa/hcp/resource.go
@@ -795,6 +795,10 @@ func (r *ClusterRosaHcpResource) Update(ctx context.Context, request resource.Up
 		}
 	}
 
+	if toPatch, shouldPatch := common.ShouldPatchString(state.AWSBillingAccountID, plan.AWSBillingAccountID); shouldPatch {
+		clusterBuilder.AWS(cmv1.NewAWS().BillingAccountID(toPatch))
+	}
+
 	clusterSpec, err := clusterBuilder.Build()
 	if err != nil {
 		response.Diagnostics.AddError(

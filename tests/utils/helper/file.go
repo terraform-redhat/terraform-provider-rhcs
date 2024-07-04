e371f945fb07597ac23600e8cc48b6ebdbbcbd28
radtriste <tradisso@redhat.com>
2024-07-04 10:01:20 +0200

    OCM-9358 | ci: TF destroy should ignore missing tfvars file

--- tests/utils/helper/file.go
+++ tests/utils/helper/file.go
@@ -8,3 +8,14 @@ import (
 func DeleteFile(filename string) error {
 	return os.Remove(filename)
 }
+
+func IsFileExists(filePath string) (bool, error) {
+	_, err := os.Stat(filePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
+}

661104da257d797dc1663d76abae1aa58feedbf3
Xue Li <xueli@redhat.com>
2024-08-13 15:16:22 +0800

    OCM-10343 | ci: Fix the sensitive leak of CA and billing account

--- tests/utils/log/constants.go
+++ tests/utils/log/constants.go
@@ -11,6 +11,8 @@ const (
 var RedactKeyList = []*regexp.Regexp{
 	regexp.MustCompile(`(\\?"password\\?":\\?")([^"]*)(\\?")`),
 	regexp.MustCompile(`(\\?"additional_trust_bundle\\?":\\?")([^"]*)(\\?")`),
-	regexp.MustCompile(`(-----BEGIN CERTIFICATE-----\n)([^-----]*)(-----END CERTIFICATE-----)`),
+	regexp.MustCompile(`(-----BEGIN CERTIFICATE-----)([^-----]*)(-----END CERTIFICATE-----)`),
 	regexp.MustCompile(`(password\s*=\s*)([^\n\\\n]+)([\n\\\n]+)`),
+	regexp.MustCompile(`(aws_(billing)?_?account_id[\s]*=[\s\\]*"?)([0-9]{12})([\\"]*)`),
+	regexp.MustCompile(`(arn:aws:[^:]*:[a-z0-9-]*:)([0-9]{12})([^\n\"\\]*)`),
 }

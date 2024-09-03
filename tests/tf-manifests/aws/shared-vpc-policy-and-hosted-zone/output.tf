aa7f261c60215c09f21f6e13ce055b6130083804
radtriste <tradisso@redhat.com>
2024-09-03 15:21:20 +0200

    OCM-10873 | ci: Use shared vpc module in tests

--- tests/tf-manifests/aws/shared-vpc-policy-and-hosted-zone/output.tf
+++ tests/tf-manifests/aws/shared-vpc-policy-and-hosted-zone/output.tf
@@ -1,18 +1,14 @@
 output "shared_role" {
   description = "Shared VPC Role ARN"
-  value       = time_sleep.shared_resources_propagation.triggers["shared_vpc_role_arn"]
+  value       = module.shared_vpc_policy_and_hosted_zone.shared_role
 }
 
 output "hosted_zone_id" {
   description = "Hosted Zone ID"
-  value       = time_sleep.shared_resources_propagation.triggers["shared_vpc_hosted_zone_id"]
+  value       = module.shared_vpc_policy_and_hosted_zone.hosted_zone_id
 }
 
 output "shared_subnets" {
   description = "The Amazon Resource Names (ARN) of the resource share"
-  value       = [for resource_arn in aws_ram_resource_association.shared_vpc_resource_association[*].resource_arn : trimprefix(resource_arn, local.resource_arn_prefix)]
-}
-
-output "azs" {
-  value = distinct(values(data.aws_subnet.shared_subnets)[*].availability_zone)
+  value       = module.shared_vpc_policy_and_hosted_zone.shared_subnets
 }

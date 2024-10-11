7e5a186b842c11bcc2c08a14a6cb83aef0b69930
radtriste <tradisso@redhat.com>
2024-10-11 14:04:37 +0200

    OCM-11804 | ci: Avoid to use hashicorp/aws 5.71.0
    Due to https://github.com/hashicorp/terraform-provider-aws/issues/39676

--- examples/create_rosa_sts_cluster/classic_sts/cluster/main.tf
+++ examples/create_rosa_sts_cluster/classic_sts/cluster/main.tf
@@ -18,7 +18,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.20.0"
+      version = ">= 4.20.0, != 5.71.0"
     }
     rhcs = {
       version = ">= 1.1.0"
--- tests/tf-manifests/aws/oidc-provider-operator-roles/rosa-hcp/main.tf
+++ tests/tf-manifests/aws/oidc-provider-operator-roles/rosa-hcp/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.20.0"
+      version = ">= 4.20.0, != 5.71.0"
     }
     rhcs = {
       version = ">= 1.6.3"

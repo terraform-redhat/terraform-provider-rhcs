d8944187d48c0e962b654956e828bbdb7a43554b
Marco <llan@redhat.com>
2025-11-13 01:34:58 -0600

    OCM-19843 | fix: support external auth in cluster template

--- tests/tf-manifests/rhcs/clusters/rosa-hcp/main.tf
+++ tests/tf-manifests/rhcs/clusters/rosa-hcp/main.tf
@@ -84,6 +84,7 @@ resource "rhcs_cluster_rosa_hcp" "rosa_hcp_cluster" {
   disable_waiting_in_destroy                      = var.disable_waiting_in_destroy
   registry_config                                 = var.registry_config
   worker_disk_size                                = var.worker_disk_size
+  external_auth_providers_enabled                 = var.external_auth_providers_enabled
 }
 
 resource "rhcs_cluster_wait" "rosa_cluster" { # id: 71869

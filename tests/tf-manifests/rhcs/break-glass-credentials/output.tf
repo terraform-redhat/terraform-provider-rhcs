76590456920f6779abc1c6c3700a1a14592f9e72
Marco <llan@redhat.com>
2025-11-18 01:33:04 -0600

    OCM-19843 | feat: add e2e test for break glass credential resource

---
+++ tests/tf-manifests/rhcs/break-glass-credentials/output.tf
@@ -0,0 +1,25 @@
+output "kubeconfig" {
+  value = rhcs_break_glass_credential.rosa_break_glass_credential.kubeconfig
+  sensitive = true
+}
+
+output "id" {
+  value = rhcs_break_glass_credential.rosa_break_glass_credential.id
+}
+
+output "username" {
+  value = rhcs_break_glass_credential.rosa_break_glass_credential.username
+}
+
+output "expiration_timestamp" {
+  value = rhcs_break_glass_credential.rosa_break_glass_credential.expiration_timestamp
+}
+
+output "revocation_timestamp" {
+  value = rhcs_break_glass_credential.rosa_break_glass_credential.revocation_timestamp
+}
+
+output "status" {
+  value = rhcs_break_glass_credential.rosa_break_glass_credential.status
+}
+

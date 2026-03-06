c1749d4478470f1dfc1d98b4b49ffe38273285ba
lufreita <lufreita@redhat.com>
2026-03-06 10:44:09 -0300

    OCM-22690 | fix: migrated e2e AWS helper utilities to aws sdk v2

--- tests/utils/helper/aws.go
+++ tests/utils/helper/aws.go
@@ -1,49 +1,54 @@
 package helper
 
 import (
+	"context"
 	"fmt"
 	"regexp"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	awsconfig "github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
 )
 
 // TODO: Remove this once this bug is fixed (https://issues.redhat.com/browse/OCPBUGS-74960)
 func DeleteExtraSecurityGroups(region string, vpcId string) error {
-	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String(region),
-	})
+	ctx := context.Background()
+	cfg, err := awsconfig.LoadDefaultConfig(ctx, awsconfig.WithRegion(region))
 	if err != nil {
 		return err
 	}
-	client := ec2.New(sess)
+	client := ec2.NewFromConfig(cfg)
 
-	var securityGroups []*ec2.SecurityGroup
-	params := ec2.DescribeSecurityGroupsInput{
-		Filters: []*ec2.Filter{
+	var securityGroups []types.SecurityGroup
+	paginator := ec2.NewDescribeSecurityGroupsPaginator(client, &ec2.DescribeSecurityGroupsInput{
+		Filters: []types.Filter{
 			{
 				Name:   aws.String("vpc-id"),
-				Values: []*string{&vpcId},
+				Values: []string{vpcId},
 			},
 		},
-	}
-	err = client.DescribeSecurityGroupsPages(&params, func(page *ec2.DescribeSecurityGroupsOutput, lastPage bool) bool {
-		securityGroups = append(securityGroups, page.SecurityGroups...)
-		return !lastPage
 	})
+	for paginator.HasMorePages() {
+		page, err := paginator.NextPage(ctx)
+		if err != nil {
+			return err
+		}
+		securityGroups = append(securityGroups, page.SecurityGroups...)
+	}
+
 	namePattern, err := regexp.Compile(`^\w{32}-vpce-private-router$`)
 	if err != nil {
 		return err
 	}
 	for _, securityGroup := range securityGroups {
-		if namePattern.MatchString(*securityGroup.GroupName) {
+		if namePattern.MatchString(aws.ToString(securityGroup.GroupName)) {
 			// Found a matching extra security group
-			out, err := client.DeleteSecurityGroup(&ec2.DeleteSecurityGroupInput{
+			_, err := client.DeleteSecurityGroup(ctx, &ec2.DeleteSecurityGroupInput{
 				GroupId: securityGroup.GroupId,
 			})
 			if err != nil {
-				return fmt.Errorf("Failed to delete security group '%s': %s", *securityGroup.GroupId, out.String())
+				return fmt.Errorf("failed to delete security group '%s': %w", aws.ToString(securityGroup.GroupId), err)
 			}
 		}
 	}
--- tests/utils/helper/iam_policy_fetcher.go
+++ tests/utils/helper/iam_policy_fetcher.go
@@ -1,12 +1,14 @@
 package helper
 
 import (
+	"context"
 	"encoding/json"
 	"net/url"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/iam"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	awsconfig "github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/iam"
+	"github.com/aws/aws-sdk-go-v2/service/iam/types"
 )
 
 type IAMPolicy struct {
@@ -21,19 +23,23 @@ type IAMPolicyStatement struct {
 }
 
 func GetRoleAttachedPolicies(roleName string) ([]IAMPolicy, error) {
-	sess := session.Must(session.NewSession())
-	svc := iam.New(sess)
+	ctx := context.Background()
+	cfg, err := awsconfig.LoadDefaultConfig(ctx)
+	if err != nil {
+		return nil, err
+	}
+	svc := iam.NewFromConfig(cfg)
 
 	var policies []IAMPolicy
 
 	// List attached policies
-	attachedPolicies, err := listAttachedRolePolicies(svc, roleName)
+	attachedPolicies, err := listAttachedRolePolicies(ctx, svc, roleName)
 	if err != nil {
 		return nil, err
 	}
 
 	for _, policy := range attachedPolicies {
-		policyDoc, err := getPolicyDocument(svc, *policy.PolicyArn)
+		policyDoc, err := getPolicyDocument(ctx, svc, aws.ToString(policy.PolicyArn))
 		if err != nil {
 			return nil, err
 		}
@@ -43,26 +49,30 @@ func GetRoleAttachedPolicies(roleName string) ([]IAMPolicy, error) {
 	return policies, nil
 }
 
-func listAttachedRolePolicies(svc *iam.IAM, roleName string) ([]*iam.AttachedPolicy, error) {
-	var attachedPolicies []*iam.AttachedPolicy
+func listAttachedRolePolicies(ctx context.Context, svc *iam.Client, roleName string) ([]types.AttachedPolicy, error) {
+	var attachedPolicies []types.AttachedPolicy
 	input := &iam.ListAttachedRolePoliciesInput{
 		RoleName: aws.String(roleName),
 	}
 
-	err := svc.ListAttachedRolePoliciesPages(input, func(page *iam.ListAttachedRolePoliciesOutput, lastPage bool) bool {
+	paginator := iam.NewListAttachedRolePoliciesPaginator(svc, input)
+	for paginator.HasMorePages() {
+		page, err := paginator.NextPage(ctx)
+		if err != nil {
+			return nil, err
+		}
 		attachedPolicies = append(attachedPolicies, page.AttachedPolicies...)
-		return !lastPage
-	})
+	}
 
-	return attachedPolicies, err
+	return attachedPolicies, nil
 }
 
-func getPolicyDocument(svc *iam.IAM, policyArn string) (IAMPolicy, error) {
+func getPolicyDocument(ctx context.Context, svc *iam.Client, policyArn string) (IAMPolicy, error) {
 	input := &iam.GetPolicyInput{
 		PolicyArn: aws.String(policyArn),
 	}
 
-	result, err := svc.GetPolicy(input)
+	result, err := svc.GetPolicy(ctx, input)
 	if err != nil {
 		return IAMPolicy{}, err
 	}
@@ -72,7 +82,7 @@ func getPolicyDocument(svc *iam.IAM, policyArn string) (IAMPolicy, error) {
 		VersionId: result.Policy.DefaultVersionId,
 	}
 
-	versionResult, err := svc.GetPolicyVersion(policyVersionInput)
+	versionResult, err := svc.GetPolicyVersion(ctx, policyVersionInput)
 	if err != nil {
 		return IAMPolicy{}, err
 	}
